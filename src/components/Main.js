cdfbabd6ed649e384fb6e65c8e794bf91fa2d156
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-09-27 10:55:31 -0400

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index.js";
+import Show from "../pages/Show.js";
+
+function Main(props) {
+    
+    const [ people, setPeople ] = useState(null);
+    const URL = "http://localhost:4000/people";
+
+    const getPeople = async () => {
+        const repsonse = await fetch(URL);
+        const data = await repsonse.json();
+        setPeople(data);
+    }
+
+    const createPeople = async (person) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person)
+        })
+        //update list of people    
+        getPeople();
+    }
+    //works
+    //useEffect(() => () => getPeople(), []);
+    useEffect(() => {getPeople()}, []);
+
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index 
+                        people={people}
+                        createPeople={createPeople}
+                        />
+                </Route>
+                <Route 
+                    path="/people/:id"
+                    render={(rp) => (
+                        <Show
+                        {...rp}
+                        />
+                    )}
+                />
+            </Switch>
+        </main>
+    )
+};
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//IMPORT SCSS Files to be the source of style
+import "./styles.scss";
+//Import Router
+import { BrowserRouter as Router } from 'react-router-dom';
+//import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
